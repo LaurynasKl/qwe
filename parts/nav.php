66aa233fcec51eb64b2c1ae797853fb12b793737
Laurynas <klumbyslaurynas@gmail.com>
2024-07-03 11:07:32 +0300

    qwe

---
+++ parts/nav.php
@@ -0,0 +1,10 @@
+
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="http://localhost/qwe/read.php">CRUD</a>
+        <div  class="navbar-nav">
+        <a class="nav-link" href="http://localhost/qwe/read.php">Read</a>
+        <a class="nav-link" href="http://localhost/qwe/create.php">Create</a>
+        </div>
+    </div>
+</nav>
